537d073ba889823d5ef621c232baacb52718deaf
Frank Albenesius <frankalbenesius@gmail.com>
2020-02-17 19:31:35 -0600

    Use .env for api key.

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
+var API_KEY = process.env.REACT_APP_API_KEY;
 var CLIENT_ID =
   "749312304038-dj5vsd6bgdc35f02bfvg5h091q6b28b5.apps.googleusercontent.com";
-var API_KEY = "AIzaSyCSLiwWAQF203xb1iwWwK_7wDZIypWHytY";
 var DISCOVERY_DOCS = [
   "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
 ];
 var SCOPES = [
-  //   "https://www.googleapis.com/auth/calendar.readonly",
+  "https://www.googleapis.com/auth/calendar.readonly",
   "https://www.googleapis.com/auth/admin.directory.resource.calendar.readonly"
 ].join(" ");
 
