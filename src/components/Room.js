83c8ca7e6ebeb041d361c2b592ab5664633c169d
Frank Albenesius <frankalbenesius@gmail.com>
2020-02-24 19:46:12 -0600

    add font

--- src/components/Room.js
+++ src/components/Room.js
@@ -5,9 +5,12 @@ import useRoomAvailability from "../hooks/useRoomAvailability";
 
 function Room({ id, name, capacity, isSelected, onClick }) {
   const { isAvailable } = useRoomAvailability(id);
+
   let backgroundColor = "#eee";
+  let availabilityMessage = "...";
   if (isAvailable !== null) {
     backgroundColor = isAvailable ? "LightGreen" : "LightPink";
+    availabilityMessage = isAvailable ? "Available" : "Unavailable";
   }
 
   return (
@@ -17,16 +20,24 @@ function Room({ id, name, capacity, isSelected, onClick }) {
         background: ${backgroundColor};
         padding: 0.5rem;
         border-right: 0.25em solid white;
+        margin-bottom: 0.1rem;
         ${isSelected && `border-color: black;`}
       `}
     >
       <div
         className={css`
-          font-weight: bold;
           text-transform: uppercase;
         `}
       >
         {name} ({capacity})
+        <div
+          className={css`
+            font-size: 0.8rem;
+            text-transform: capitalize;
+          `}
+        >
+          {availabilityMessage}
+        </div>
       </div>
     </div>
   );
